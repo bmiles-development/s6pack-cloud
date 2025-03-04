e1dee1a0dc3bb58a57c294a6f43a8e8d16d9768a
bmilesp <bmilesp@gmail.com>
2025-03-04 13:30:05 -0500

    revert

--- stacks/hosting/infrastructure/aws/ssm.ts
+++ stacks/hosting/infrastructure/aws/ssm.ts
@@ -1,6 +1,6 @@
 
 import { DataAwsCallerIdentity } from '@cdktf/provider-aws/lib/data-aws-caller-identity';
-import { SsmParameter } from '@cdktf/provider-aws/lib/ssm-parameter';
+import { DataAwsSsmParameter } from '@cdktf/provider-aws/lib/data-aws-ssm-parameter';
 import { Construct } from 'constructs';
 export class Ssm extends Construct {
     private _parameters : any = {}
@@ -13,81 +13,57 @@ export class Ssm extends Construct {
     constructor(scope: Construct, name: string, parameterStorePrefix:string) {
         super(scope,name)
 
-        this._parameters['testUsername'] = new SsmParameter(this, "testUsername",{
-            name : parameterStorePrefix+"testUsername",
-            type: "SecureString",
-            value: ""
+        this._parameters['testUsername'] = new DataAwsSsmParameter(this, "testUsername",{
+            name : parameterStorePrefix+"testUsername"
         }).value
-
-        this._parameters['testPassword'] = new SsmParameter(this, "testPassword",{
-            name : parameterStorePrefix+"testPassword",
-            type: "SecureString",
-            value: ""
+             
+        this._parameters['testPassword'] = new DataAwsSsmParameter(this, "testPassword",{
+            name : parameterStorePrefix+"testPassword"
+         }).value
+             
+        this._parameters['testUserPoolId'] = new DataAwsSsmParameter(this, "testUserPoolId",{
+            name : parameterStorePrefix+"testUserPoolId"
         }).value
-
-        this._parameters['testUserPoolId'] = new SsmParameter(this, "testUserPoolId",{
-            name : parameterStorePrefix+"testUserPoolId",
-            type: "SecureString",
-            value: ""
+             
+        this._parameters['testCognitoClientId'] = new DataAwsSsmParameter(this, "testCognitoClientId",{
+            name : parameterStorePrefix+"testCognitoClientId"
         }).value
-
-        this._parameters['testCognitoClientId'] = new SsmParameter(this, "testCognitoClientId",{
-            name : parameterStorePrefix+"testCognitoClientId",
-            type: "SecureString",
-            value: ""
+             
+        this._parameters['stripeToken-dev'] = new DataAwsSsmParameter(this, "stripeTokenDev",{
+            name : parameterStorePrefix+"stripeToken-dev"
         }).value
-
-        this._parameters['stripeToken-dev'] = new SsmParameter(this, "stripeTokenDev",{
-            name : parameterStorePrefix+"stripeToken-dev",
-            type: "SecureString",
-            value: ""
+             
+        this._parameters['stripeToken-live'] = new DataAwsSsmParameter(this, "stripeTokenLive",{
+            name : parameterStorePrefix+"stripeToken-live"
         }).value
-
-        this._parameters['stripeToken-live'] = new SsmParameter(this, "stripeTokenLive",{
-            name : parameterStorePrefix+"stripeToken-live",
-            type: "SecureString",
-            value: ""
+             
+        this._parameters['contactUsEmail-dev'] = new DataAwsSsmParameter(this, "contactUsEmailDev",{
+            name : parameterStorePrefix+"contactUsEmail-dev"
         }).value
-
-        this._parameters['contactUsEmail-dev'] = new SsmParameter(this, "contactUsEmailDev",{
-            name : parameterStorePrefix+"contactUsEmail-dev",
-            type: "SecureString",
-            value: ""
+             
+        this._parameters['contactUsEmail-live'] = new DataAwsSsmParameter(this, "contactUsEmailLive",{
+            name : parameterStorePrefix+"contactUsEmail-live"
         }).value
-
-        this._parameters['contactUsEmail-live'] = new SsmParameter(this, "contactUsEmailLive",{
-            name : parameterStorePrefix+"contactUsEmail-live",
-            type: "SecureString",
-            value: ""
+             
+        this._parameters['recaptchaSiteSecret-dev'] = new DataAwsSsmParameter(this, "recaptchaSiteSecretDev",{
+            name : parameterStorePrefix+"recaptchaSiteSecret-dev"
         }).value
-
-        this._parameters['recaptchaSiteSecret-dev'] = new SsmParameter(this, "recaptchaSiteSecretDev",{
-            name : parameterStorePrefix+"recaptchaSiteSecret-dev",
-            type: "SecureString",
-            value: ""
+             
+        this._parameters['recaptchaSiteSecret-live'] = new DataAwsSsmParameter(this, "recaptchaSiteSecretLive",{
+            name : parameterStorePrefix+"recaptchaSiteSecret-live"
         }).value
-
-        this._parameters['recaptchaSiteSecret-live'] = new SsmParameter(this, "recaptchaSiteSecretLive",{
-            name : parameterStorePrefix+"recaptchaSiteSecret-live",
-            type: "SecureString",
-            value: ""
+             
+        this._parameters['stripeWebhookSigningSecret-dev'] = new DataAwsSsmParameter(this, "stripeWebhookSigningSecretDev",{
+            name : parameterStorePrefix+"stripeWebhookSigningSecret-dev"
         }).value
-
-        this._parameters['stripeWebhookSigningSecret-dev'] = new SsmParameter(this, "stripeWebhookSigningSecretDev",{
-            name : parameterStorePrefix+"stripeWebhookSigningSecret-dev",
-            type: "SecureString",
-            value: ""
-        }).value
-
-        this._parameters['stripeWebhookSigningSecret-live'] = new SsmParameter(this, "stripeWebhookSigningSecretLive",{
-            name : parameterStorePrefix+"stripeWebhookSigningSecret-live",
-            type: "SecureString",
-            value: ""
+             
+        this._parameters['stripeWebhookSigningSecret-live'] = new DataAwsSsmParameter(this, "stripeWebhookSigningSecretLive",{
+            name : parameterStorePrefix+"stripeWebhookSigningSecret-live"
         }).value
 
         //get acountId and add it to parameters
         const identity = new DataAwsCallerIdentity(this, "identity")
         this._parameters['accountId'] = identity.accountId
-    
+
     }
 }
